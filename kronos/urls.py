093efa990e27aed9caa3a4c5234643df6fa8b0df
Nathan Chapman <nathanchapman@protonmail.ch>
2015-11-10 19:09:32 -0800

    Retrieved some semblance of what happened before.

--- kronos/urls.py
+++ kronos/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.overview, name="overveiw"),
-    url(r'^timecard/add/$', views.add_timecard, name="add timecard"),
-    url(r'^timecards/completed/$', views.completed, name="completed"),
-    url(r'^timecard/(?P<timecard_id>[0-9]+)/$', views.detail, name="detail"),
-    url(r'^timecard/(?P<timecard_id>[0-9]+)/review/$', views.review, name="review"),
-    url(r'^timecard/(?P<timecard_id>[0-9]+)/update/$', views.update, name="update"),
-    #url(r'^(?P<timecard_id>[0-9]+)/delete/$', views.delete, name="delete"),
+    url(r'^$', views.overview, name="overview"),
+    url(r'^add/$', views.add_timecard, name="add timecard"),
+    url(r'^complete/$', views.complete, name="complete"),
+    url(r'^(?P<timecard_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<timecard_id>[0-9]+)/review/$', views.review, name="review"),
+    url(r'^(?P<timecard_id>[0-9]+)/update/$', views.update, name="update"),
+    url(r'^(?P<timecard_id>[0-9]+)/delete/$', views.deletetc, name="deletetc"),
+    url(r'^(?P<timecard_id>[0-9]+)/task/add/$', views.add_task, name="addtask"),
 ]
--- kronos/views.py
+++ kronos/views.py
@@ -2,11 +2,20 @@ from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Timecard, Task
 from django.contrib.auth.models import User
+from hq import models as hq_models
 
+# /kronos/
+# Overview of all timecards
 def overview(request):
-    tcs = Timecard.objects.all().filter(complete=False)
-    return render(request, 'kronos/overview.html', {'timecards': tcs})
+    tcss = Timecard.objects.all()
+    tcs = tcss.filter(complete=False)
+    ctcs = tcss.filter(complete=True)[:3]
+    return render(request, 'kronos/overview.html', {
+        'timecards': tcs,
+        'complete_timecards': ctcs
+        })
 
+# /kronos/timecard/add/
 def add_timecard(request):
     # if POST process data and create timecard
     if request.method == "POST":
@@ -28,6 +37,7 @@ def add_timecard(request):
     else:
         return render(request, 'kronos/addtimecard.html')
 
+# /kronos/1/
 def detail(request, timecard_id):
     try:
         tc = Timecard.objects.get(pk=timecard_id)
@@ -39,6 +49,7 @@ def detail(request, timecard_id):
         'tasks': tasks
         })
 
+# /kronos/1/review
 # have to be a manager
 def review(request, timecard_id):
     try:
@@ -47,13 +58,60 @@ def review(request, timecard_id):
         raise Http404("Timecard does not exist.")
     return render(request, 'kronos/review.html', {'timecard': tc})
 
-def completed(request):
-    pass
+# 
+# return a list of completed timecards
+def complete(request):
+    tc = Timecard.objects.order_by('date').filter(complete=True)
+    return render(request, 'kronos/complete.html', {'timecard': tc})
 
 def update(request, timecard_id):
     pass
 
-#def edit(request, timecard_id):
+# /kronos/task/add/
+def add_task(request, timecard_id):
+    # if POST process data and create timecard
+    if request.method == "POST":
+        employee = request.POST["employee"]
+        date = request.POST["date"]
+        pr = request.POST["project"]
+        description = request.POST["description"]
+        start_time = request.POST["start_time"]
+        end_time = request.POST["end_time"]
+        labor_item_number = request.POST["labor_item_number"]
+        li_class = request.POST["li_class"]
 
+        project = hq_models.Project.objects.get(id=pr)
+        user = User.objects.get(id=employee)
+        timecard = Timecard.objects.get(id=timecard_id)
 
-#def delete(request, timecard_id):
+        t = Task.objects.create(
+            employee=user,
+            timecard=timecard,
+            date = date,
+            project = project,
+            description = description,
+            start_time = start_time,
+            end_time = end_time,
+            labor_item_number = labor_item_number,
+            li_class = li_class
+            )
+        t.save()
+
+        return HttpResponseRedirect('/kronos/')
+
+    else:
+        p = hq_models.Project.objects.all()
+        return render(request, 'kronos/addtask.html', {
+            'projects': p,
+            'timecard_id': timecard_id
+            })
+
+def edit(request, timecard_id):
+    pass
+
+# /kronos/1/delete
+# deletes the timecard with id
+def deletetc(request, timecard_id):
+    t = Timecard.objects.get(pk=timecard_id)
+    t.delete()
+    return HttpResponseRedirect('/kronos/')    
