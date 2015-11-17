79cf0edea3cb6b4c1f5fc60f7f7519d9462c8dc7
Nathan Chapman <nathanchapman@protonmail.ch>
2015-11-17 11:29:00 -0800

    Ready for deployment.

--- workroom/settings.py
+++ workroom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ssvn$_^nfy-psrb9la83h@dg0hg=wbq^jo-y%wm=f3%t-(4+_s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nathanjc.pythonanywhere.com']
 
@@ -114,3 +114,13 @@ EMAIL_PORT = 1025
 DEFAULT_FROM_EMAIL = 'webmaster@wallyworks.net'
 LOGIN_REDIRECT_URL = '/kronos/'
 LOGOUT_URL = '/login/'
+
+SECURE_HSTS_SECONDS = 30
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
